aaeb27fae3f086f5695129ed550d5c94c7f04d41
astrofra <astrofra@gmail.com>
2014-07-14 21:48:21 +0200

    - Temp commit.

--- Assets/faces_all_palettes.h
+++ Assets/faces_all_palettes.h
@@ -1,6 +1,6 @@
 #include <exec/types.h>
 #include <intuition/intuition.h>
 
-extern int faces_all_index[5385];
+extern int faces_all_index[5744];
 extern UWORD faces_all_PaletteRGB4[32];
 extern UWORD faces_all_scanline_PaletteRGB4[5712];
