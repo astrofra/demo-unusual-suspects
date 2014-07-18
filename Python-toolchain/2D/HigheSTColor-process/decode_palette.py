82e9eec964097e222dd47f3a0593e2f864a7cf2a
astrofra <astrofra@gmail.com>
2014-07-18 08:24:58 +0200

    - New "faces all" image.

--- Python-toolchain/2D/HigheSTColor-process/decode_palette.py
+++ Python-toolchain/2D/HigheSTColor-process/decode_palette.py
@@ -3,7 +3,7 @@ import codecs
 import shutil
 import os
 
-input_root_filename = 'data360_97'
+input_root_filename = 'data360_996'
 palette_filename = input_root_filename + '.pal'
 index_filename = input_root_filename + '.idx'
 root_name = 'faces_all'
