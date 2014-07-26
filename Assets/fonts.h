5f9b48fd27c5fa82792bd8a469d8fa13c444e898
astrofra <astrofra@gmail.com>
2014-07-26 19:55:44 +0200

    - Fonts file, draft version.

--- Assets/fonts.h
+++ Assets/fonts.h
@@ -1,4 +1,4 @@
 /* Font descriptor headers */
 
-char future_font_glyph_array[94];
-int future_font_x_pos_array[94];
+extern const char future_font_glyph_array[88];
+extern const int future_font_x_pos_array[90];
