71387fc26048202bb1f3ae4d23a868402ea90da1
astrofra <astrofra@gmail.com>
2014-08-07 14:22:23 +0200

    - English editing by Sven 'Ptoing' R.

--- demo_strings.c
+++ demo_strings.c
@@ -1,20 +1,20 @@
 /*	Unusual Suspects 
 	Various Strings */
 
-const char *desc_char_0 = "Nasuhl Sardik\n\2Home planet :\1Glokul VI\n\2Blood type :\1 various types\ncan be collected on his\nhands.\n\2Occupation :\1 remove people^s\nhead, barehandedly.\0";
-const char *desc_char_1 = "Eggdrop Leonardh\n\2Home planet :\1 Fishia XII\n\2Residence :\1 State Prison,\nDraconia\n\2Occupation :\1 emits a strong\nsmell when put under heavy\npressure.\0";
-const char *desc_char_2 = "Golem Mosenthal\n\2Home planet :\1 Foxxcon IV\n\2Occupation :\n\1 - Process data\n - Kill humans\nCharged with diversion of robotic\nlaws 1, 2 and 3.\0";
-const char *desc_char_3 = "Bjorn Thorson\n\2Blood type :\1 A-Blue\n\2Occupation :\1 Forest\nassets manager\nSuspected of a massive\nmissapropriation of bamboos.\0";
-const char *desc_char_4 = "Claude Bayou\n\2Address :\1 Louisiana, Earth\n\2Occupation :\n\1 - Eat flies\n - Cook Cajun cuisine\nWanted for flies genocide.\0";
-const char *desc_char_5 = "Sandra Nyan\nHome planet :\1 undefined\n\2Occupation :\n\1 - go outside\n - go inside\n            - repeat step #1\nConvicted of acute selfishness.\0";
-const char *desc_char_6 = "Neuron X. Boll\n\2Home planet :\1 Optikol VII\n\2Occupation :\n\1motion prediction for the\nDivx industry.\nAccused for large scale piracy\nby the attorneys.\0";
+const char *desc_char_0 = "Nasuhl Sardik\n\2Home planet :\1Glokul VI\n\2Blood type :\1 Various types\n blended together\n on his hands.\n\2Occupation :\1 Remove heads, bare handedly.\0";
+const char *desc_char_1 = "Eggdrop Leonardh\n\2Home planet :\1 Ghoti XII\n\2Residence :\1 State Prison,\nDraconia\n\2Occupation :\1 Emitting a strong\n odor when put under\npressure.\0";
+const char *desc_char_2 = "Golem Mosenthal\n\2Home planet :\1 Foxxcon IV\n\2Occupation :\n\1 - Process data\n - Kill humans\nCharged with violation of\n all three Laws of Robotics.\0";
+const char *desc_char_3 = "Bjorn Thorson\n\2Blood type :\1 A-Blue\n\2Occupation :\1 Forest\nassets manager\nSuspected of massive\nmisappropriation of bamboo.\0";
+const char *desc_char_4 = "Claude Bayou\n\2Address :\1 Louisiana, Earth\n\2Occupation :\n\1 - Eat flies\n - Cook Cajun cuisine\nWanted for fly genocide.\0";
+const char *desc_char_5 = "Sandra Nyan\nHome planet :\1 undefined\n\2Occupation :\n\1 - Go outside\n - Go inside\n            - Repeat step #1\nFound guilty of acute selfishness.\0";
+const char *desc_char_6 = "Neuron X. Boll\n\2Home planet :\1 Optikol VII\n\2Occupation :\n\1Motion prediction for the\nDivX industry.\nAccused of large scale piracy.\0";
 const char *desc_char_7 = "Prime Minister Maya\nSpace Soviet Union Gov.\n\2Occupation :\n\1 - President of SSU\n - Prime Minister of SSU\n             - General of SSU Army\0";
-const char *desc_char_8 = "Paul Levion\n\2Address :\1 Brusells, Earth\n\2Occupation :\1 space aviation\npionneer and national hero.\nSuspected of war crimes,\nbombing, abduction, gassing.\nDenies evidences.\0";
-const char *desc_char_9 = "Felex Jagger\n\2Home planet :\1 Galligay\n\2Occupation :\1 Mentalist\nKnown for having\nhypnotized a whole\npopulation of robots from the planet\nFoxxcon IV.\0";
-const char *desc_char_10 = "U-Head\n\2Home planet :\1 NES 13\n\2Blood type :\1 A+\n\2Occupation :\1 Corrupt data\non every magnetic tapes\nfound within a range of 150 metres.\nChargxd fpr t#*x$$$*$*$$$\0";
-const char *desc_char_11 = "Sweety Cheung\n\2Address :\1 Long Island Zoo\n\2Occupation :\n\1 - Former gargoyle\n - Full time alien animal\nWanted since he escaped from\nthe Washington National Cathedral.\0";
+const char *desc_char_8 = "Paul Levion\n\2Address :\1 Brusells, Earth\n\2Occupation :\1 Space aviation\npionneer and national hero.\nSuspected of war crimes,\nbombing, abduction, gassing.\nDenies all culpability.\0";
+const char *desc_char_9 = "Felex Jagger\n\2Home planet :\1 Galligay\n\2Occupation :\1 Mentalist\nKnown for having\nhypnotized a large\nnumber of robots from planet\nFoxxcon IV.\0";
+const char *desc_char_10 = "U-Head\n\2Home planet :\1 unknown\n\2Blood type :\1 unknown \n\2Occupation :\1 Indefinite hiatus\nFound guilty for not being\nin active development.\0";
+const char *desc_char_11 = "Sweety Cheung\n\2Address :\1 Long Island Zoo\n\2Occupation :\n\1 - Former gargoyle\n - Full time alien animal\nWanted since escape from\nthe Washington National Cathedral.\0";
 const char *desc_char_12 = "Eckon 004\n\2Home planet :\1 Ledger VI\n\2Blood type : Cold+\n\2Occupation :\n\1High Frequency Trader\nMost wanted since the great\nReal Estate crash in 2068.\0";
-const char *desc_char_13 = "Eva #2F4\n\2Home planet :\1 Arborea\n\2Occupation : \1Great Master\nof environmentalism.\nCharged for eco-terrorism.\0";
-const char *desc_char_14 = "Feather Magnum\n\2Address :\n\1Never left his bedroom.\n\2Occupation :\n\1Stalking lolitas on the\nvarious social networks.\0";
-const char *desc_char_15 = "Rebecka D. Vulvor\n\2Home planet : \1Gama IV\n\2Occupation : \1\nCurrator of the Planetary\nEmbryo Museum.\0";
-const char *desc_char_16 = "Lester K. Chaykin\n\2Home planet :\1 Earth\n\2Occupation :\n\1 - Physicist\n - Adventurer\nCharged with the destruction of the\nLarge Hardon Collider, Geneva.\0";
+const char *desc_char_13 = "Eva #2F4\n\2Home planet :\1 Arborea\n\2Occupation : \1Great Master\nof environmentalism.\nCharged with eco-terrorism.\0";
+const char *desc_char_14 = "Feather Magnum\n\2Address :\n\1Never left his bedroom.\n\2Occupation :\n\1Stalking underage girls on\nvarious social networks.\0";
+const char *desc_char_15 = "Rebecka D. Vulvor\n\2Home planet : \1Ovum IV\n\2Occupation : \1\nCurator of the Planetary\nEmbryo Museum.\0";
+const char *desc_char_16 = "Lester K. Chaykin\n\2Home planet :\1 Earth\n\2Occupation :\n\1 - Physicist\n - Adventurer\nCharged with the destruction of the\nLarge Hadron Collider, Geneva.\0";
